c87a40843e89c60ecdefd143b07854eb277c467d
hernancaceres <harinatha777@hotmail.com>
2024-09-27 09:27:49 -0300

    backend corriendo en local actualizacion2

--- src/config.js
+++ src/config.js
@@ -2,10 +2,11 @@
 import dotenv from "dotenv";
 dotenv.config();
 
-export const FRONTEND_URL = process.env.FRONTEND_URL || "http://localhost:5173";
-export const DB_HOST = process.env.DB_HOST || "localhost";
-export const DB_PORT = process.env.DB_PORT || 5432;
-export const DB_DATABASE = process.env.DB_DATABASE || "sigilodb";
-export const DB_USER = process.env.DB_USER || "postgres";
-export const DB_PASSWORD = process.env.DB_PASSWORD || "v4juuK4fUPYoguaK";
-export const PORT = process.env.PORT || 4000;
+
+export const FRONTEND_URL = process.env.FRONTEND_URL 
+export const DB_HOST = process.env.DB_HOST 
+export const DB_PORT = process.env.DB_PORT 
+export const DB_DATABASE = process.env.DB_DATABASE 
+export const DB_USER = process.env.DB_USER 
+export const DB_PASSWORD = process.env.DB_PASSWORD 
+export const PORT = process.env.PORT 
